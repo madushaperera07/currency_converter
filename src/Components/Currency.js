7d7b4de1a13e1f2268cd062bf3baeea7d62091b6
madushaperera07 <madushanilakshan9@gmail.com>
2023-08-02 20:11:58 +0530

    update dropdown

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DropdownL from "./DropdownL";
 import DropdownR from "./DropdownR";
-
+import axios from "axios";
 import image from "../img/images.png";
 
 function Currency() {
+  const [CurrencyVal, setCurrencyVal] = useState("");
+  const [DropdownRight, setDropdownRight] = useState("");
+
+  const [rates, setRates] = useState([]);
+
+  async function submit(e) {
+    e.preventDefault();
+    console.log(CurrencyVal);
+    console.log(DropdownRight);
+  }
+
+  useEffect(() => {
+    axios.get(`https://api.frankfurter.app/latest`).then((res) => {
+      setRates([res.data.base, ...Object.keys(res.data.rates)]);
+    });
+  }, []);
+  console.log(rates);
+
   return (
     <div class="full_container">
       <div class="currency_container">
@@ -12,18 +30,33 @@ function Currency() {
           <img src={image} />
           <h3>CURRENCY CONVERTER</h3>
         </div>
-        <form>
+        <form action="POST">
           <div class="input_container">
-            <input placeholder="Enter the amount" />
+            <input
+              placeholder="Enter the amount"
+              name="text"
+              onChange={(e) => {
+                setCurrencyVal(e.target.value);
+              }}
+            />
           </div>
 
           <div class="dropdown_container">
             <DropdownL />
-            <DropdownR />
+            <DropdownR
+              rates={rates}
+              onchangeCurrency={(e) => {
+                setDropdownRight(e.target.value);
+              }}
+            />
           </div>
 
           <div class="button_container">
-            <input type="submit" class="btn btn-success px-3 button" />
+            <input
+              type="submit"
+              onClick={submit}
+              class="btn btn-success px-3 button"
+            />
           </div>
         </form>
 
--- src/Components/DropdownR.js
+++ src/Components/DropdownR.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-function DropdownR() {
+function DropdownR(props) {
+  const { rates, onchangeCurrency } = props;
   return (
     <div>
       <select
         class="form-select form-select-sm w-20 DropdownR"
         aria-label=".form-select-sm example"
+        onChange={onchangeCurrency}
       >
-        <option selected>To</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        {rates.map((convert) => (
+          <option key={convert} value={convert}>
+            {convert}
+          </option>
+        ))}
       </select>
     </div>
   );
